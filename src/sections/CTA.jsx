4d25237fc41cac5571832d7492e6e48e215c114e
slaygun <nabhraghav@gmail.com>
2024-02-17 12:51:06 +0530

    glow on cta

--- src/sections/CTA.jsx
+++ src/sections/CTA.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function CTA() {
   return (
     <section id="contact" className="container py-10">
-      <div className="bg-gradient-to-r from-violet-400 via-blue-500 to-indigo-600 bg-primary py-10 px-10 text-center rounded-xl space-y-10">
+      <div className="shadow-[7px_7px_153px_10px_rgba(45,_72,_210,_0.52)] bg-gradient-to-r from-violet-400 via-blue-500 to-indigo-600 bg-primary py-10 px-10 text-center rounded-xl space-y-10">
         <h1 className="text-4xl md:text-8xl font-extrabold mb-4">Start your journey with LyfeLynk</h1>
         <div className="inline-flex space-x-4 pt-5">
           <Button variant="secondary">Contact Us</Button>
