984b068f772edaa57aa6e42c1ac35b4c89a52112
slaygun <nabhraghav@gmail.com>
2024-02-20 13:50:50 +0530

    small tweaks left  for responsiveness

--- src/sections/AboutUs.jsx
+++ src/sections/AboutUs.jsx
@@ -36,9 +36,9 @@ const teamData = [
 
 export default function AboutUs() {
   return (
-    <section id="about" className="w-full py-12 md:py-24 lg:py-32">
+    <section className="w-full py-12 md:py-24 lg:py-32">
 
-      <div className="container px-4 grid items-center justify-center gap-4 text-center md:px-6 lg:gap-10">
+      <div id="about" className="container px-4 grid items-center justify-center gap-4 text-center md:px-6 lg:gap-10">
         <div className="space-y-3">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">About Us</h2>
           <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
