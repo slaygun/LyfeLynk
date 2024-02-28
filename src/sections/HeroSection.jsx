9ec9d3cddb6a266537d1b7d36613292a53016b7e
slaygun <nabhraghav@gmail.com>
2024-02-28 09:53:23 +0530

    hero fix

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -7,13 +7,13 @@ import AnimatedSection from "@/components/AnimatedSection";
 export default function Component() {
   return (
     
-    <section className="px-4 py-5 md:py-10">
+    <section className="min-h-screen px-4 py-10">
       
       <div class="circlePosition w-11/12 h-11/12 bg-[#367ed142] rounded-[100%] absolute -z-10 blur-[100px] flex justify-center items-center">
         <div class="circle w-[17rem] h-[17rem] bg-[#5743ee42] rounded-[100%]" />
       </div>
 
-      <div className="min-h-screen max-w-4xl mx-auto flex flex-col items-center text-center">
+      <div className="max-w-4xl mx-auto flex flex-col items-center text-center">
 
         <h1 class="text-4xl font-bold tracking-tighter md:text-8xl mb-6">
           Digitally <span class="bg-gradient-to-tl from-yellow-500 via-blue-500 to-indigo-500 text-transparent bg-clip-text">Linking</span> your Health.
